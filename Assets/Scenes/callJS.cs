19481f14fbfd32e56bf48259c8b12d03b9363c7e
Legrain Samuel <samuel.legrain@bstorm.be>
2020-11-26 12:47:07 +0100

    Initiate test

---
+++ Assets/Scenes/callJS.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class callJS : MonoBehaviour
+{
+    static int value = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Move(int direction)
+    {
+        switch (direction)
+        {
+            case 1:
+                transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+                break;
+            case 2:
+                transform.position = new Vector2(transform.position.x - 1, transform.position.y);
+                break;
+            case 3:
+                transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+                break;
+            case 4:
+                transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+                break;
+        }
+    }
+}
